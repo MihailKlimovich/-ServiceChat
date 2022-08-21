9ef8c6d7bd6734cecb15d3be552e7b88bad9d5af
Mihail Klimovich <mishaklimovich87@gmail.com>
2022-08-21 23:55:57 +0300

    Added changes

--- components/signupScreen/SignupForm.js
+++ components/signupScreen/SignupForm.js
@@ -6,23 +6,23 @@ import {
     Pressable,
     TouchableOpacity,
     Alert,
-} from 'react-native';
-import React, { useState } from 'react';
-import { firebase, db } from '../../firebase';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import Validator from 'email-validator';
+} from "react-native";
+import React, { useState } from "react";
+import { firebase, db } from "../../firebase";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Validator from "email-validator";
 
 const SignupForm = ({ navigation }) => {
     const SignupFormSchema = Yup.object().shape({
-        email: Yup.string().email().required('An email is required'),
-        username: Yup.string().required().min(2, 'A username is required'),
+        email: Yup.string().email().required("An email is required"),
+        username: Yup.string().required().min(2, "A username is required"),
         password: Yup.string()
             .required()
-            .min(6, 'Your password has to at least 6 characters'),
+            .min(6, "Your password has to at least 6 characters"),
     });
     const getRandomProfilePicture = async () => {
-        const responce = await fetch('https://randomuser.me/api');
+        const responce = await fetch("https://randomuser.me/api");
         const data = await responce.json();
         return data.results[0].picture.large;
     };
@@ -31,24 +31,26 @@ const SignupForm = ({ navigation }) => {
             const authUser = await firebase
                 .auth()
                 .createUserWithEmailAndPassword(email, password);
-            console.log('====================================');
-            console.log('Firebase User Created Successfully', email, password);
-            console.log('====================================');
+            console.log("====================================");
+            console.log("Firebase User Created Successfully", email, password);
+            console.log("====================================");
 
-            db.collection('users').add({
-                owner_uid: authUser.user.uid,
-                username: username,
-                email: authUser.user.email,
-                profile_picture: await getRandomProfilePicture(),
-            });
+            db.collection("users")
+                .doc(authUser.user.email)
+                .set({
+                    owner_uid: authUser.user.uid,
+                    username: username,
+                    email: authUser.user.email,
+                    profile_picture: await getRandomProfilePicture(),
+                });
         } catch (error) {
-            Alert.alert('My friend ...', error.message);
+            Alert.alert("My friend ...", error.message);
         }
     };
     return (
         <View style={styles.wrapper}>
             <Formik
-                initialValues={{ email: '', username: '', password: '' }}
+                initialValues={{ email: "", username: "", password: "" }}
                 onSubmit={(values) => {
                     onSignup(values.email, values.password, values.username);
                 }}
@@ -70,20 +72,20 @@ const SignupForm = ({ navigation }) => {
                                     borderColor:
                                         values.email.length < 1 ||
                                         Validator.validate(values.email)
-                                            ? '#ccc'
-                                            : 'red',
+                                            ? "#ccc"
+                                            : "red",
                                 },
                             ]}
                         >
                             <TextInput
-                                placeholderTextColor='#444'
-                                placeholder='Phone number, username or email'
-                                autoCapitalize='none'
-                                keyboardType='email-address'
-                                textContentType='emailAddress'
+                                placeholderTextColor="#444"
+                                placeholder="Phone number, username or email"
+                                autoCapitalize="none"
+                                keyboardType="email-address"
+                                textContentType="emailAddress"
                                 autoFocus={true}
-                                onChangeText={handleChange('email')}
-                                onBlur={handleBlur('email')}
+                                onChangeText={handleChange("email")}
+                                onBlur={handleBlur("email")}
                                 value={values.email}
                             />
                         </View>
@@ -94,20 +96,20 @@ const SignupForm = ({ navigation }) => {
                                     borderColor:
                                         values.username.length < 1 ||
                                         values.username.length >= 2
-                                            ? '#ccc'
-                                            : 'red',
+                                            ? "#ccc"
+                                            : "red",
                                 },
                             ]}
                         >
                             <TextInput
-                                placeholderTextColor='#444'
-                                placeholder='Username'
-                                autoCapitalize='none'
-                                keyboardType='username'
-                                textContentType='emailAddress'
+                                placeholderTextColor="#444"
+                                placeholder="Username"
+                                autoCapitalize="none"
+                                keyboardType="username"
+                                textContentType="emailAddress"
                                 autoFocus={true}
-                                onChangeText={handleChange('username')}
-                                onBlur={handleBlur('username')}
+                                onChangeText={handleChange("username")}
+                                onBlur={handleBlur("username")}
                                 value={values.username}
                             />
                         </View>
@@ -118,21 +120,21 @@ const SignupForm = ({ navigation }) => {
                                     borderColor:
                                         values.password.length < 1 ||
                                         values.password.length >= 6
-                                            ? '#ccc'
-                                            : 'red',
+                                            ? "#ccc"
+                                            : "red",
                                 },
                             ]}
                         >
                             <TextInput
-                                placeholderTextColor='#444'
-                                placeholder='Password'
-                                autoCapitalize='none'
+                                placeholderTextColor="#444"
+                                placeholder="Password"
+                                autoCapitalize="none"
                                 autoCorrect={false}
                                 secureTextEntry={true}
-                                keyboardType='email-address'
-                                textContentType='password'
-                                onChangeText={handleChange('password')}
-                                onBlur={handleBlur('password')}
+                                keyboardType="email-address"
+                                textContentType="password"
+                                onChangeText={handleChange("password")}
+                                onBlur={handleBlur("password")}
                                 value={values.password}
                             />
                         </View>
@@ -149,8 +151,8 @@ const SignupForm = ({ navigation }) => {
                             <TouchableOpacity
                                 onPress={() => navigation.goBack()}
                             >
-                                <Text style={{ color: '#6BB0F5' }}>
-                                    {' '}
+                                <Text style={{ color: "#6BB0F5" }}>
+                                    {" "}
                                     Log In
                                 </Text>
                             </TouchableOpacity>
@@ -169,28 +171,28 @@ const styles = StyleSheet.create({
     inputField: {
         borderRadius: 4,
         padding: 12,
-        backgroundColor: '#FAFAFA',
+        backgroundColor: "#FAFAFA",
         marginBottom: 10,
         borderWidth: 1,
-        justifyContent: 'center',
+        justifyContent: "center",
     },
     button: (isValid) => ({
         marginTop: 50,
-        backgroundColor: isValid ? '#0096F6' : '#9ACAF7',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: isValid ? "#0096F6" : "#9ACAF7",
+        alignItems: "center",
+        justifyContent: "center",
         minHeight: 42,
         borderRadius: 4,
     }),
     buttonText: {
-        fontWeight: '600',
-        color: '#fff',
+        fontWeight: "600",
+        color: "#fff",
         fontSize: 20,
     },
     signupContainer: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'center',
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "center",
         marginTop: 50,
     },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,15 +4,21 @@ import {
     ScrollView,
     Platform,
     StatusBar,
-} from "react-native";
-import React from "react";
-import Header from "../components/home/Header";
-import Stories from "../components/home/Stories";
-import Post from "../components/home/Post";
-import { POSTS } from "../data/post";
-import BottomTabs, { bottomTabIcons } from "../components/home/BottomTabs";
+} from 'react-native';
+import React, { useEffect } from 'react';
+import Header from '../components/home/Header';
+import Stories from '../components/home/Stories';
+import Post from '../components/home/Post';
+import { POSTS } from '../data/post';
+import BottomTabs, { bottomTabIcons } from '../components/home/BottomTabs';
+import { db } from '../firebase';
 
 const HomeScreen = ({ navigation }) => {
+    useEffect(() => {
+        db.collectionGroup('posts').onSnapshot((snapshot) => {
+            console.log(snapshot.docs.map((doc) => doc.data()));
+        });
+    }, []);
     return (
         <SafeAreaView style={styles.container}>
             <Header navigation={navigation} />
@@ -29,9 +35,9 @@ const HomeScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "black",
+        backgroundColor: 'black',
         flex: 1,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     },
 });
 
